ccab05250c7a2d3cf13e4336c7751724b403c94e
Claudio Andrés Roman <c.romanbecerra@gmail.com>
2024-12-04 21:54:58 -0300

    x

--- Base/settings.py
+++ Base/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'Cerradura2024.$/'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
